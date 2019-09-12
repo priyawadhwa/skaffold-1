26801126816bbdd88b45b6b04e49b3e1d6813de1
Priya Wadhwa <priyawadhwa@google.com>
2019-09-12 18:19:56 -0400

    testing'

--- examples/getting-started/main.go
+++ examples/getting-started/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"time"
 )
 
 func main() {
+	var s string
+	contents, err := ioutil.ReadFile("file")
+	if err != nil {
+		panic(err)
+	}
+	s = string(contents)
+	i := 0
 	for {
 		fmt.Println("Hello world!")
-
+		if i < 11 {
+			s = s + s
+			i = i + 1
+		}
 		time.Sleep(time.Second * 1)
 	}
 }
